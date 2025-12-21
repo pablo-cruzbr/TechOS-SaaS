7789e71bdb3985468798a6878dd0ab1546e9df68
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-21 12:34:37 -0300

    fix: explicit use of memoryStorage to bypass EROFS

--- SF2/Backend/src/config/multer.ts
+++ SF2/Backend/src/config/multer.ts
@@ -1,19 +1,15 @@
-import crypto from 'crypto'
+import crypto from 'crypto';
 import multer from 'multer';
 
 export default {
-    upload() { 
-        return {
-            storage: multer.memoryStorage(), 
-            limits: {
-                fileSize: 5 * 1024 * 1024, // Limite de 5MB
-            },
-            
-            filename: (request: any, file: any, callback: any) => {
-                const fileHash = crypto.randomBytes(16).toString("hex");
-                const fileName = `${fileHash}-${file.originalname}`;
-                return callback(null, fileName);
-            }
-        }
-    }
-}
+  upload() { 
+    return {
+      storage: multer.memoryStorage(), 
+      filename: (request: any, file: any, callback: any) => {
+        const fileHash = crypto.randomBytes(16).toString("hex");
+        const fileName = `${fileHash}-${file.originalname}`;
+        return callback(null, fileName);
+      }
+    };
+  }
+};
