0571d0449cd72d555d368d221db7b430662794d3
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-12-22 17:14:03 -0300

    fix: build successful - dynamic rendering applied to dashboard

--- SF2/Frontend/src/services/api.ts
+++ SF2/Frontend/src/services/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 //  baseURL: "http://localhost:3334",
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  //baseURL: "http://localhost:3334",
   headers: {
     "Content-Type": "application/json",
   },
