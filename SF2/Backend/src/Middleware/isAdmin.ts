15d1346fa2ccb9e6c4b3ebcf9b86a3ac1eb66e03
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-01-07 10:16:07 -0300

    feat: Implementando middleware de isAdmin

---
+++ SF2/Backend/src/Middleware/isAdmin.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import prismaClient from "../prisma";
+
+export async function isAdmin(req: Request, res: Response, next: NextFunction) {
+    const user_id = req.user_id;
+
+    const user = await prismaClient.user.findUnique({
+        where: {id: user_id},
+        select: {isAdmin: true}
+    });
+
+    if(!user?.isAdmin) {
+        return res.status(403).json({error: "Acesso restrito a administrtatores."})
+    }
+    return next();
+}
