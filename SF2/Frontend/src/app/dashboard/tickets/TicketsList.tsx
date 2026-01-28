a162e2f0f6e246e0feb6b84805f1ffefbda13f85
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-01-28 08:34:09 -0300

    feat: remove icon trash

--- SF2/Frontend/src/app/dashboard/tickets/TicketsList.tsx
+++ SF2/Frontend/src/app/dashboard/tickets/TicketsList.tsx
@@ -206,10 +206,10 @@ export default function TicketsList({ ticketsData }: Props) {
               <p className={`${styles.field} ${styles.data}`}>
                 Data: {ticket.created_at ? new Date(ticket.created_at).toLocaleString("pt-BR", { day: "2-digit", month: "2-digit", hour: "2-digit", minute: "2-digit" }) : ""}
               </p>
-              <FaRegTrashAlt
+              {/*<FaRegTrashAlt
                 onClick={(e) => { e.stopPropagation(); handleDeleteCardTecnico(ticket.id); }}
                 className={styles.iconTrash}
-              />
+              />*/}
             </div>
           </div>
         ))}
